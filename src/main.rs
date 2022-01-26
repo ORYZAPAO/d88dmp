afbe5c894a64a2a7a9cb860bfcbb56b56ce7ece9
ORYZAPAO <oryzapao@gmail.com>
2022-01-26 21:23:00 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 mod cli;
+mod d88_header;
 mod report_d88;
+mod utility;
+mod version;
 
 fn main() {
     // Command Line Analysis
-    let cmdline_info = cli::cli();
+    let cmdline_info = cli::get_cmdline_param();
 
     let mut rpt_d88 = report_d88::ReportD88::new(cmdline_info);
 
---
+++ src/utility.rs
@@ -0,0 +1,83 @@
+use ansi_term::Color;
+//use super::report_d88::ReportD88;
+use crate::report_d88::ReportD88;
+
+impl ReportD88 {
+    /// Print Track Bar(Helper function)
+    ///
+    /// タイトルバーを表示
+    ///
+    pub fn print_track_bar(&self) {
+        println!("Track Table");
+        println!("hex  dec  +0    +1    +2    +3    +4    +5    +6    +7   ");
+        print!("--- ----  -----------------------------------------------");
+    }
+
+    /// Print Tiltle Bar(Helper function)
+    ///
+    /// タイトルバーを表示
+    ///
+    pub fn print_title_bar(&self) {
+        println!("Offst +0 +1 +2 +3 +4 +5 +6 +7 +8 +9 +a +b +c +d +e +f                 ");
+        println!("----- -----------------------------------------------                 ");
+    }
+
+    /// Print 16byte (Helper function)
+    ///
+    /// 16byte表示
+    /// D88ファイルのオフセット情報`offset` と 16byte生データ`buf16`から、16byteを整形して表示する。  
+    ///
+    /// # Argument
+    ///
+    ///   * `buf16` Slice to 16 byte Buffer
+    ///   * `offset` Offset at D88 Disk File
+    ///
+    /// # Return
+    ///
+    ///   * Return the value of `offset` plus 16.
+    ///
+    pub fn print_16byte(&mut self, buf16: &[u8], offset: usize, color: ansi_term::Color) -> usize {
+        let mut char_pat = [
+            '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.',
+        ];
+
+        // Offset Address
+        //
+        if !self.nocolor_flg {
+            print!("{} ", Color::Cyan.paint(&(format!("{:05x} ", offset))));
+        } else {
+            print!("{} ", format!("{:05x}", offset));
+        }
+
+        // 16 byte
+        //
+        let mut byte16_str = String::from("");
+        for i in 0..16 {
+            unsafe {
+                if libc::isprint(buf16[i] as libc::c_int) != 0 {
+                    char_pat[i] = buf16[i] as char;
+                }
+            }
+
+            byte16_str.push_str(&(format!("{:02x} ", buf16[i])));
+        }
+        //print!("{}", Color::White.paint(byte16_str));
+
+        if !self.nocolor_flg {
+            print!("{}", color.paint(byte16_str));
+        } else {
+            print!("{}", byte16_str);
+        }
+
+        // Character
+        //
+        for p in char_pat.iter() {
+            print!("{}", p);
+        }
+        print!(" ");
+
+        //
+        //
+        offset + 16
+    }
+}
