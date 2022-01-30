29cd9e0823829b519100bcac8698e9d2981de51c
ORYZAPAO <oryzapao@gmail.com>
2022-01-31 00:57:38 +0900

    Rename d88info -> d88dmp

--- src/version.rs
+++ src/version.rs
@@ -3,5 +3,5 @@
 //
 
 pub const AUTHOR: &str = "ORYZA (https://github.com/ORYZAPAO)";
-pub const TOOL_NAME: &str = "d88info";
+pub const TOOL_NAME: &str = "d88dmp";
 pub const TOOL_VERSION: &str = "ver 0.12.1";
