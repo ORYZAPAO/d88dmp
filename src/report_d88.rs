28d9fe25dfe849ebbd2e3137aaba909f8a6a6556
ORYZAPAO <oryzapao@gmail.com>
2022-02-03 22:26:27 +0900

    Add read_d88_header(), read_sector_header()

--- src/report_d88.rs
+++ src/report_d88.rs
@@ -1,6 +1,7 @@
 /// Report D88 File
 use std::fs;
 use std::io::{BufReader, Read};
+use std::io::{Seek, SeekFrom};
 use std::mem;
 
 use ansi_term::Color;
@@ -30,6 +31,72 @@ impl ReportD88 {
             noinfo_flg: _noinfo_flg,
             nocolor_flg: _nocolor_flg,
             //fh: None,
+        }
+    }
+
+    /// Read D88 Header (Helper function)
+    ///
+    /// D88ファイルのヘッダ情報を返す
+    ///
+    /// # Argument
+    ///
+    ///   * `reader` BufferReader instance at D88 File
+    ///
+    /// # Return
+    ///
+    ///   * Result<D88_Header, ()>
+    ///
+    pub unsafe fn read_d88_header(
+        &mut self,
+        reader: &mut BufReader<std::fs::File>,
+    ) -> Result<D88_Header, ()> {
+        let mut buf: [u8; mem::size_of::<D88_Header>()] = [0; mem::size_of::<D88_Header>()]; // Header Buffer
+
+        if let Ok(_) = reader.seek(SeekFrom::Start(0)) {
+            if let Ok(read_size) = reader.read(&mut buf) {
+                if read_size != mem::size_of::<D88_Header>() {
+                    return Err(());
+                }
+                Ok(mem::transmute::<
+                    [u8; mem::size_of::<D88_Header>()],
+                    D88_Header,
+                >(buf))
+            } else {
+                Err(())
+            }
+        } else {
+            Err(())
+        }
+    }
+
+    /// Read Sector Header (Helper function)
+    ///
+    /// セクタのヘッダ情報を返す
+    ///
+    /// # Argument
+    ///
+    ///   * `reader` BufferReader instance at D88 File
+    ///
+    /// # Return
+    ///
+    ///   * Result<D88_SectirHdr, ()>
+    ///
+    pub unsafe fn read_sector_header(
+        &mut self,
+        reader: &mut BufReader<std::fs::File>,
+    ) -> Result<D88_SectorHdr, ()> {
+        let mut buf: [u8; mem::size_of::<D88_SectorHdr>()] = [0; mem::size_of::<D88_SectorHdr>()]; // Header Buffer
+
+        if let Ok(read_size) = reader.read(&mut buf) {
+            if read_size != mem::size_of::<D88_SectorHdr>() {
+                return Err(());
+            }
+            Ok(mem::transmute::<
+                [u8; mem::size_of::<D88_SectorHdr>()],
+                D88_SectorHdr,
+            >(buf))
+        } else {
+            Err(())
         }
     }
 
@@ -96,74 +163,76 @@ impl ReportD88 {
     ///   * if Error, return 0
     ///
     pub fn report_d88_header(&mut self, reader: &mut BufReader<std::fs::File>) -> usize {
-        let mut buf: [u8; mem::size_of::<D88_Header>()] = [0; mem::size_of::<D88_Header>()]; // Header Buffer
+        // Get File Header
+        let header_rslt;
+        unsafe {
+            header_rslt = self.read_d88_header(reader);
+        }
 
-        // File Header
-        //
-        if let Ok(read_size) = reader.read(&mut buf) {
-            unsafe {
-                let header = mem::transmute::<[u8; mem::size_of::<D88_Header>()], D88_Header>(buf);
+        if let Ok(header) = header_rslt {
+            // Output Track Table
+            //
+            self.print_track_bar();
+            for n in 0..164 {
+                if (n % 8) == 0 {
+                    println!("");
 
-                // Report D88 Header
-                //
-
-                // Track Table
-                //
-                self.print_track_bar();
-                for n in 0..164 {
-                    if (n % 8) == 0 {
-                        println!("");
-
-                        if self.nocolor_flg {
-                            print!(
-                                "{}  {:05x} ",
-                                (format!("{0:2x}h {0:3}d", n)),
-                                header.track_tbl[n]
-                            );
-                        } else {
-                            print!(
-                                "{}  {:05x} ",
-                                Color::Cyan.paint(format!("{0:2x}h {0:3}d", n)),
-                                header.track_tbl[n]
-                            );
-                        }
+                    if self.nocolor_flg {
+                        print!(
+                            "{}  {:05x} ",
+                            (format!("{0:2x}h {0:3}d", n)),
+                            header.track_tbl[n]
+                        );
                     } else {
-                        print!("{:05x} ", header.track_tbl[n]);
+                        print!(
+                            "{}  {:05x} ",
+                            Color::Cyan.paint(format!("{0:2x}h {0:3}d", n)),
+                            header.track_tbl[n]
+                        );
                     }
+                } else {
+                    print!("{:05x} ", header.track_tbl[n]);
                 }
-
-                // Data
-                //
-                println!("");
-                println!("");
-                self.print_title_bar();
-                self.print_16byte(&buf, 0x0000, ansi_term::Color::Green);
-
-                //let converted: String = String::from_utf8_unchecked( header.disk_name.to_vec()).unwrap();
-                let disk_name: String = String::from_utf8_unchecked(header.disk_name.to_vec());
-                print!("Name({}), ", disk_name);
-                print!(
-                    "WriteProtect({}), ",
-                    match header.write_protect {
-                        0x10 => "Protected",
-                        0x00 => "None",
-                        _ => "!! Illegal !!",
-                    }
-                );
-                print!(
-                    "Type({} Disk), ",
-                    match header.disk_type {
-                        0x00 => "2D",
-                        0x10 => "2DD",
-                        0x20 => "2HD",
-                        _ => "??",
-                    }
-                );
-                print!("DiskSize({} byte)", header.disk_size);
             }
+
+            // Output Data
+            //
+            println!("");
+            println!("");
+            let byte_img;
+            unsafe {
+                byte_img = mem::transmute::<D88_Header, [u8; mem::size_of::<D88_Header>()]>(header);
+            }
+
+            self.print_title_bar();
+            self.print_16byte(&byte_img, 0x0000, ansi_term::Color::Green);
+
+            let disk_name;
+            unsafe {
+                disk_name = String::from_utf8_unchecked(header.disk_name.to_vec());
+            }
+            print!("Name({}), ", disk_name);
+            print!(
+                "WriteProtect({}), ",
+                match header.write_protect {
+                    0x10 => "Protected",
+                    0x00 => "None",
+                    _ => "!! Illegal !!",
+                }
+            );
+            print!(
+                "Type({} Disk), ",
+                match header.disk_type {
+                    0x00 => "2D",
+                    0x10 => "2DD",
+                    0x20 => "2HD",
+                    _ => "??",
+                }
+            );
+            print!("DiskSize({} byte)", header.disk_size);
             println!();
 
-            read_size
+            mem::size_of::<D88_Header>()
         } else {
             0
         }
@@ -182,79 +251,77 @@ impl ReportD88 {
     ///   * if Success, return Ok(`header`)  `header` is D88_SectorHdr instance (Sector Header Info.)
     ///   * if Error, return Err("err")
     ///
-    pub unsafe fn report_sector_hdr_dat(
+    pub fn report_sector_hdr_dat(
         &mut self,
         reader: &mut BufReader<std::fs::File>,
         offset: usize,
     ) -> Result<D88_SectorHdr, &'static str> {
         // Buffer for D88 Sector Header
         //
-        let mut d88_sector_header_buf: [u8; mem::size_of::<D88_SectorHdr>()] =
-            [0; mem::size_of::<D88_SectorHdr>()];
+        let sector_hdr;
+        unsafe {
+            sector_hdr = self.read_sector_header(reader);
+        }
 
         // Sector Header
         //
-        if let Ok(rdsize) = reader.read(&mut d88_sector_header_buf) {
-            if rdsize != 0 {
-                // Report 16byte Data
-                //
-                self.print_16byte(&d88_sector_header_buf, offset, ansi_term::Color::Green);
-
-                // Report Sector Header
-                //
-                let d88_sector_header = mem::transmute::<
-                    [u8; mem::size_of::<D88_SectorHdr>()],
-                    D88_SectorHdr,
-                >(d88_sector_header_buf);
-
-                print!(
-                    "Track({}), Side({}), Sector({}), ",
-                    d88_sector_header.track, d88_sector_header.side, d88_sector_header.sec
+        if let Ok(d88_sector_header) = sector_hdr {
+            // Report 16byte Data
+            //
+            unsafe {
+                let byte_img = mem::transmute::<D88_SectorHdr, [u8; mem::size_of::<D88_SectorHdr>()]>(
+                    d88_sector_header,
                 );
-
-                print!("Size({} byte/sec), ", 128 << (d88_sector_header.sec_size));
-                print!(
-                    "NumOfSec({} sec/track), ",
-                    d88_sector_header.number_of_sector
-                );
-                print!(
-                    "Status({}), ",
-                    match d88_sector_header.status {
-                        0x00 => "OK",           // 正常
-                        0x10 => "DELETED",      // 削除済みデータ
-                        0xa0 => "ID CRC Err",   // ID CRC エラー
-                        0xb0 => "Data CRC Err", // データ CRC エラー
-                        0xe0 => "No Addr Mark", // アドレスマークなし
-                        0xf0 => "No Data Mark", // データマークなし
-                        _ => "??",
-                    }
-                );
-                print!(
-                    "Density({:02x}h), ",
-                    //match d88_sector_header.density {
-                    //    0x00 => "D",  // 倍密度 // dencityの仕様がよく分からない。
-                    //    0x40 => "S",  // 単密度
-                    //    0x01 => "HD", // 高密度
-                    //    _ => "??",
-                    //}
-                    d88_sector_header.density
-                );
-                print!(
-                    "Mark({}), ",
-                    match d88_sector_header.deleted_mark {
-                        0x00 => "NORMAL",
-                        0x10 => "DELETED",
-                        _ => "??",
-                    }
-                );
-                print!("DataSize({} byte), ", d88_sector_header.size_of_data);
-
-                println!();
-
-                Ok(d88_sector_header) // [u8;16] --> D88_SectorHdr
-            } else {
-                Err("err")
+                self.print_16byte(&byte_img, offset, ansi_term::Color::Green);
             }
+
+            // Report Sector Header
+            //
+            print!(
+                "Track({}), Side({}), Sector({}), ",
+                d88_sector_header.track, d88_sector_header.side, d88_sector_header.sec
+            );
+
+            print!("Size({} byte/sec), ", 128 << (d88_sector_header.sec_size));
+            print!(
+                "NumOfSec({} sec/track), ",
+                d88_sector_header.number_of_sector
+            );
+            print!(
+                "Status({}), ",
+                match d88_sector_header.status {
+                    0x00 => "OK",           // 正常
+                    0x10 => "DELETED",      // 削除済みデータ
+                    0xa0 => "ID CRC Err",   // ID CRC エラー
+                    0xb0 => "Data CRC Err", // データ CRC エラー
+                    0xe0 => "No Addr Mark", // アドレスマークなし
+                    0xf0 => "No Data Mark", // データマークなし
+                    _ => "??",
+                }
+            );
+            print!(
+                "Density({:02x}h), ",
+                //match d88_sector_header.density {
+                //    0x00 => "D",  // 倍密度 // dencityの仕様がよく分からない。
+                //    0x40 => "S",  // 単密度
+                //    0x01 => "HD", // 高密度
+                //    _ => "??",
+                //}
+                d88_sector_header.density
+            );
+            print!(
+                "Mark({}), ",
+                match d88_sector_header.deleted_mark {
+                    0x00 => "NORMAL",
+                    0x10 => "DELETED",
+                    _ => "??",
+                }
+            );
+            print!("DataSize({} byte), ", d88_sector_header.size_of_data);
+
+            println!();
+
+            Ok(d88_sector_header) // [u8;16] --> D88_SectorHdr
         } else {
             Err("err")
         }
@@ -281,7 +348,7 @@ impl ReportD88 {
         let mut offset = offset_;
         let sector_size;
 
-        // Read Header
+        // Read Sector Header
         //
         if let Ok(d88_sector_header) = self.report_sector_hdr_dat(reader, offset) {
             // Sector Header Info
@@ -291,7 +358,7 @@ impl ReportD88 {
             return 0;
         }
 
-        // Read Secrtor
+        // Read Secrtor Data
         //
         let mut sector_buffer = vec![0; sector_size]; // Get Sector Buffer
         let mut rdsize = reader.read(&mut sector_buffer).unwrap();
