54cf3a899dca50f7c1456ea8da851cd31b74ee39
ORYZAPAO <oryzapao@gmail.com>
2022-02-07 01:17:49 +0900

    Update

--- d88dmp/src/version.rs
+++ d88dmp/src/version.rs
@@ -4,4 +4,4 @@
 
 pub const AUTHOR: &str = "ORYZA (https://github.com/ORYZAPAO)";
 pub const TOOL_NAME: &str = "d88dmp";
-pub const TOOL_VERSION: &str = "ver 0.12.3";
+pub const TOOL_VERSION: &str = "ver 0.0.2";
