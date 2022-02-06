ff7e86c9ac8df5d4c3614d4c7223f278cf8902b3
ORYZAPAO <oryzapao@gmail.com>
2022-02-06 20:25:45 +0900

    Update

--- crates/D88FileIO/src/lib.rs
+++ crates/D88FileIO/src/lib.rs
@@ -3,9 +3,9 @@ pub mod format;
 
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+    //#[test]
+    //fn it_works() {
+    //    let result = 2 + 2;
+    //    assert_eq!(result, 4);
+    //}
 }
